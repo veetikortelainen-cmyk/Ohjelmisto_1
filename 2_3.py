9d26d124e19acfd44a2cf65f1f6661b2a441f090
veetikortelainen-cmyk <veetikortelainen05@gmail.com>
2025-08-20 15:48:52 +0300

    Tehtävä: 2_3

---
+++ 2_3.py
@@ -0,0 +1,9 @@
+kanta = float(input("Anna suorakulmion kanta: "))
+korkeus = float(input("Anna suorakulmion korkeus: "))
+
+# laksetaan suorakulmion piiri ja pinta-ala
+ala = kanta * korkeus
+piiri = 2*kanta + 2*korkeus
+# Tulostetaan vastaus
+
+print(f"Suorakulmion piiri on {piiri} ja pinta-ala on {ala}")
