423ba0f11cfcad1b722e10103a4a81198b3abe8e
veetikortelainen-cmyk <veetikortelainen05@gmail.com>
2025-08-20 15:57:08 +0300

    Tehtävä: 2_4

---
+++ 2_4.py
@@ -0,0 +1,13 @@
+print("Anna kolme kokonaislukua: ")
+
+luku1 = float(input("Anna Luku_1: "))
+luku2 = float(input("Anna Luku_2: "))
+luku3 = float(input("Anna Luku_3: "))
+
+summa = luku1 + luku2 + luku3
+tulo = luku1 * luku2 * luku3
+keskiarvo = (luku1 + luku2 + luku3) / 3
+
+print(f"Lukujen summa: {summa}")
+print(f"Lukujen tulo: {tulo}")
+print(f"Lukujen keskiarvo: {keskiarvo}")
