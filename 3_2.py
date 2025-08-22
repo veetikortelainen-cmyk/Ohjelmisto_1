7460c373a837f938f704726f90a2d7bf378a4e9f
veetikortelainen-cmyk <veetikortelainen05@gmail.com>
2025-08-22 16:24:12 +0300

    Tehtävä: 3_2

---
+++ 3_2.py
@@ -0,0 +1,12 @@
+luokka = input("Anna hyttiluokkasi: ")
+
+if luokka == "LUX":
+    print("LUX on parvekkeellinen hytti yläkannella.")
+elif luokka == "A":
+    print("A on ikkunallinen hytti autokannen yläpuolella.")
+elif luokka == "B":
+    print("B on ikkunaton hytti autokannen yläpuolella.")
+elif luokka == "C":
+    print("C on ikkunaton hytti autokannen alapuolella.")
+else:
+    print("Virheellinen hyttiluokka")
