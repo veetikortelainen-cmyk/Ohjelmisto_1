714cd4fcec90d3e9864eb0d2d3557aa92b817c2b
veetikortelainen-cmyk <veetikortelainen05@gmail.com>
2025-08-22 16:17:04 +0300

    Tehtävä: 3_1

---
+++ 3_1.py
@@ -0,0 +1,7 @@
+pituus = float(input("Anna kuhan pituus centtimetreinä: "))
+
+if pituus <= 37:
+    print("Kuha on liian pieni päästä kuha vapaaksi.")
+    print("Kuhan pienin pyynti pituus on 37cm")
+else:
+    print(f"Kuhansi pituus {pituus}cm")
