0e1d386918491382c2748f08eaac8fd081057abd
veetikortelainen-cmyk <veetikortelainen05@gmail.com>
2025-08-28 13:02:36 +0300

    Tehtävä: 5_4

---
+++ 5_4.py
@@ -0,0 +1,12 @@
+kaupungit = []
+print("Anna viiden kaupungin nimet: ")
+print("")
+
+for i in range(5):
+    a = input("Anna kaupungin nimi: ")
+    kaupungit.append(a)
+
+print("")
+print("Antamasi kaupungit:")
+for i in kaupungit:
+    print(i)
