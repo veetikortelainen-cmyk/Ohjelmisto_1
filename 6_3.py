35bee7545e34e50323bdcf996d7acec71d1ebfed
veetikortelainen-cmyk <veetikortelainen05@gmail.com>
2025-09-04 11:46:05 +0300

    Tehtävä: 6_3

---
+++ 6_3.py
@@ -0,0 +1,8 @@
+def gallon():
+    return 3.785 * a
+
+a = int(input("Anna bensan määrä galooneina: "))
+while a > 0:
+    litra = gallon()
+    print(litra)
+    a = int(input("Anna bensan määrä galooneina: "))
