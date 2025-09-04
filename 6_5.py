e4caf6937b4cd8ac7d0ba67b2534724f856a4b99
veetikortelainen-cmyk <veetikortelainen05@gmail.com>
2025-09-04 13:37:21 +0300

    Tehtävä: 6_5

---
+++ 6_5.py
@@ -0,0 +1,24 @@
+def yht():
+    print(f"Antamiesi numeroiden lista: {lista}")
+    print(f"Antamiesi numeroiden lista: {dif_by_twolist}")
+    return
+
+lista = []
+dif_by_twolist = []
+
+a = input("Anna kokonais luku: ")
+
+while a != "":
+    if a == "":
+        a = input("Anna kokonais luku: ")
+    else:
+        luku= int(a)
+        if luku % 2 == 0:
+            dif_by_twolist.append(luku)
+            lista.append(luku)
+            a = input("Anna kokonais luku: ")
+        else:
+            lista.append(luku)
+            a = input("Anna kokonais luku: ")
+yht()
+
