ebf559b63b7ad6cd347d00c6e887e8cb6ef5a2bc
veetikortelainen-cmyk <veetikortelainen05@gmail.com>
2025-09-14 13:21:40 +0300

    Tehtävä: 8_2

---
+++ 8_2.py
@@ -0,0 +1,31 @@
+import mysql.connector
+
+yhteys = mysql.connector.connect(
+    host='127.0.0.1',
+    port=3306,
+    database='flight_game',
+    user='root',
+    password='Moi12345',
+    autocommit=True
+)
+
+a = input("Maakoodi: ")
+maakoodi = str.upper(a)
+
+sql = f'''
+    select type, count(*) 
+    fom airport 
+    where iso_country = "{maakoodi }"
+    group by type;
+'''
+
+kursori = yhteys.cursor()
+kursori.execute(sql)
+tulos = kursori.fetchall()
+
+if kursori.rowcount > 0:
+    print(f"Lentokentät maassa {maakoodi }:")
+    for tyyppi, maara in tulos:
+        print(f" - {tyyppi}: {maara} kpl")
+else:
+    print("Ei tulostetta")
