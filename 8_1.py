9289616de28b6f970dfafef431ffb3213db1b27b
veetikortelainen-cmyk <veetikortelainen05@gmail.com>
2025-09-16 14:28:26 +0300

    Tehtävä: 8_1

--- 8_1.py
+++ 8_1.py
@@ -12,23 +12,16 @@ yhteys = mysql.connector.connect(
          autocommit=True
          )
 
-# Vaihe 2
-# luodaan sql.kysely jolla haetaan ICOA-koodi ohjelma tulostaa koodin yhteydessä olevan lentokentän
 
-a = input("Anna lentokentän ISOA-koodi: ")
-ICOA = str.upper(a)
+a = input("Anna lentokentän ISAO-koodi: ")
+ICAO = str.upper(a)
 
-sql = f'select * from airport where gps_code = "{ICOA}"'
+sql = f'select * from airport where ident = "{ICAO}"'
 
-# Vaihe 3
-# lähetetään kysely suoritettavaksi tietokantapalvelimelle
 
 kursori = yhteys.cursor()
 kursori.execute(sql)
-
 tulos = kursori.fetchall()
-if kursori._rowcount > 0:
-    for rivi in tulos:
+
+for rivi in tulos:
         print(rivi[3],"/ "+ rivi[10])
-else:
-    print("Ei tulostetta")
