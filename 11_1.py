ddf86634cdb92cd29bbe910ef18e51fa9b0277ea
veetikortelainen-cmyk <veetikortelainen05@gmail.com>
2025-10-30 09:38:39 +0200

    Tehtävä: 11_1

---
+++ 11_1.py
@@ -0,0 +1,32 @@
+class Julkaisu:
+    def __init__(self, nimi):
+        self.nimi = nimi
+
+    def tulosta_tiedot(self):
+        print(f"{self.nimi}")
+
+class Kirja(Julkaisu):
+    def __init__(self, nimi, kirjoittaja, sivumäärä):
+        super().__init__(nimi)
+        self.kirjoittaja = kirjoittaja
+        self.sivumäärä = sivumäärä
+
+    def tulosta_tiedot(self):
+        print(f"Kirja: {self.nimi}")
+        print(f"Kirjoittaja: {self.kirjoittaja}")
+        print(f"Sivumäärä: {self.sivumäärä}\n")
+
+class Lehti(Julkaisu):
+    def __init__(self, nimi, paatoimittaja):
+        super().__init__(nimi)
+        self.paatoimittaja = paatoimittaja
+
+    def tulosta_tiedot(self):
+        print(f"Lehti: {self.nimi}")
+        print(f"Päätoimittaja: {self.paatoimittaja}\n")
+
+lehti = Lehti("Aku Ankka", "Aki Hyyppä")
+kirja = Kirja("Hytti n:o 6", "Rosa Liksom", 200)
+
+lehti.tulosta_tiedot()
+kirja.tulosta_tiedot()
